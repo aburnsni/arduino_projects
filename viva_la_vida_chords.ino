d429d505a2884c2bd35af8cd6d70f93772f00d93
aburnsni <aburnsni@gmail.com>
2017-02-17 19:30:31 +0000

    Converted to mpr121

--- viva_la_vida_chords.ino
+++ viva_la_vida_chords.ino
@@ -1,46 +1,61 @@
+#include <Wire.h>
+#include "Adafruit_MPR121.h"
 #include <MIDI.h>
-#include <CapacitiveSensor.h>
+//#include <CapacitiveSensor.h>
 #include "midi_notes.h"
 
-int* song[] = {CHORD_Db, CHORD_Eb, CHORD_Ab, CHORD_Fm};
-int duration = 500;
-int instrument = 50;
+// You can have up to 4 on one i2c bus but one is enough for testing!
+Adafruit_MPR121 cap = Adafruit_MPR121();
+// Keeps track of the last pins touched
+// so we know when buttons are 'released'
+uint16_t lasttouched = 0;
+uint16_t currtouched = 0;
 
-CapacitiveSensor cs[] = {CapacitiveSensor(2, 3), CapacitiveSensor(4, 5), CapacitiveSensor(6, 7), CapacitiveSensor(8, 9)};
-long last_cap[] = {0, 0, 0, 0};
-long cap[] = {0, 0, 0, 0};
-int i = 0;
+const bool DEBUG = 0;
+int* song[] = {CHORD_Db, CHORD_Eb, CHORD_Ab, CHORD_Fm};
+//int duration = 500;
+int instrument = 50;
 
 MIDI_CREATE_DEFAULT_INSTANCE();
 void setup()
 {
+  while (!Serial);        // needed to keep leonardo/micro from starting too fast!
+
   MIDI.begin();
-  Serial.begin(115200);
-  for (i = 0; i < 4; i++) {
-    cs[i].set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on channel 1 - just as an example
-
+  if (DEBUG) {
+    Serial.begin(115200);  // needed for hairless midi
   }
-
+  if (!cap.begin(0x5A)) {
+    // Serial.println("MPR121 not found, check wiring?");
+    while (1);
+  }
+  delay(1000);
+  MIDIsoftreset();  // Midi Reset
+  delay(200);
   MIDI.sendProgramChange(instrument, 1);
 }
 
 void loop()
 {
-  for (i = 0; i < 4; i++) {
-    cap[i] = cs[i].capacitiveSensor(5);
-  }
-  for (i = 0; i < 4; i++) {
+  // Get the currently touched pads
+  currtouched = cap.touched();
 
+  for (uint8_t i = 0; i < 4; i++) {
+    // it if *is* touched and *wasnt* touched before, alert!
+    if ((currtouched & _BV(i)) && !(lasttouched & _BV(i)) ) {
+      playChord(song[i], 1);
 
-    if (abs(cap[i] - last_cap[i]) > 5000) {
-      if (cap[i] > last_cap[i]) {
-        playChord(song[i], 1);
-        delay(100);
-      }
-      //Serial.println(cap[i]);
     }
-    last_cap[i] = cap[i];
+    if (!(currtouched & _BV(i)) && (lasttouched & _BV(i)) ) {
+      stopChord(song[i], 1);
+    }
   }
+
+  // reset our state
+  lasttouched = currtouched;
+
+  // put a delay so it isn't overwhelming
+  delay(20);
 }
 
 void playChord(int i[], int channel) {
@@ -49,11 +64,21 @@ void playChord(int i[], int channel) {
       MIDI.sendNoteOn((i[note]), 100, channel);
     }
   }
-  delay(duration);
+}
+
+void stopChord(int i[], int channel) {
   for (uint8_t note = 0; note < 6; note++) {
     if (i[note]) {
       MIDI.sendNoteOff((i[note]), 100, channel);
     }
+  }
+}
+
+void MIDIsoftreset()  // switch off ALL notes on channel 1 to 16
+{
+  for (int channel = 0; channel < 16; channel++)
+  {
+    MIDI.sendControlChange(123, 0, channel);
   }
 }
 
