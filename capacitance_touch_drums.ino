17a4ed81d00e14ac819378263baf74bdb86d7f9f
Andrew Burns <aburnsni@gmail.com>
2017-02-26 16:54:52 +0000

    Added debug option for hairless-midi

--- capacitance_touch_drums.ino
+++ capacitance_touch_drums.ino
@@ -1,6 +1,8 @@
 #include <MIDI.h>
 #include <CapacitiveSensor.h>
 
+bool DEBUG = 0;
+
 int note[] = {49, 55, 52, 57};
 
 CapacitiveSensor cs[] = {CapacitiveSensor(2, 3), CapacitiveSensor(4, 5), CapacitiveSensor(6, 7), CapacitiveSensor(8, 9)};
@@ -12,7 +14,9 @@ MIDI_CREATE_DEFAULT_INSTANCE();
 void setup()
 {
   MIDI.begin();
-  Serial.begin(115200);
+  if (DEBUG) {
+    Serial.begin(115200);
+  }
   for (i = 0; i < 4; i++) {
     cs[i].set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on channel 1 - just as an example
 
