2a3eca64d5dc2976d124fa9f18196164510ea5fa
Andrew Burns <aburnsni@gmail.com>
2017-04-12 12:04:08 +0100

    Changed single status led to individual less

--- pringle_drums.ino
+++ pringle_drums.ino
@@ -2,17 +2,18 @@
 // October 2016.
 
 // For Pro Micro / Leonardo
-#define MIDI_SERIAL Serial1
+#define MIDI_SERIAL Serial
 
 static void MIDI_setup();
 static void MIDI_noteOn(int ch, int note, int velocity);
 static void MIDI_noteOff(int ch, int note);
 
-const int MIDI_CHANNEL=1;
+const int MIDI_CHANNEL = 1;
 
 const int NCHANNELS = 4;
 const int inPins[NCHANNELS] = { A0, A1, A2, A3 };
-const int midiNotes[NCHANNELS] = 
+const int ledPins[NCHANNELS] = { 3, 5, 6, 9 };
+const int midiNotes[NCHANNELS] =
 {
   // Follows General MIDI specs at https://www.midi.org/specifications/item/gm-level-1-sound-set
   36, // C3, Kick
@@ -28,26 +29,25 @@ static unsigned int trigLevel[NCHANNELS];
 static unsigned int counter[NCHANNELS] = { 0 };
 
 static unsigned int CTR_NOTEON = 10; // Roughly 5ms sampling peak voltage
-static unsigned int CTR_NOTEOFF = CTR_NOTEON + 30; // Duration roughly 15ms 
+static unsigned int CTR_NOTEOFF = CTR_NOTEON + 30; // Duration roughly 15ms
 // 0 -> not triggered
 // 1..CTR_NOTEON -> sampling note on
 // CTR_NOTEON+1 .. CTR_NOTEOFF -> note off
 
 
-static int statusPin = 2;
-
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   analogReference(DEFAULT);
-  pinMode(statusPin, OUTPUT);
-  digitalWrite(statusPin, LOW);
-  
+
   for (int i = 0; i < NCHANNELS; i++)
   {
     pinMode(inPins[i], INPUT);
     analogRead(inPins[i]);
     trigLevel[i] = thresholdLevel[i];
+    pinMode(ledPins[i], OUTPUT);
+    digitalWrite(ledPins[i], LOW);
+
   }
 
   MIDI_setup();
@@ -56,7 +56,7 @@ void setup() {
 
 void loop() {
   int ch;
-  for (ch=0; ch < NCHANNELS; ch++)
+  for (ch = 0; ch < NCHANNELS; ch++)
   {
     unsigned int v = analogRead(inPins[ch]);
     if ( counter[ch] == 0 )
@@ -65,7 +65,7 @@ void loop() {
       {
         vmax[ch] = v;
         counter[ch] = 1;
-        digitalWrite(statusPin, HIGH);
+        digitalWrite(ledPins[ch], HIGH);
       }
     }
     else
@@ -73,10 +73,10 @@ void loop() {
       if ( v > vmax[ch] )
         vmax[ch] = v;
       counter[ch]++;
-      
+
       if ( counter[ch] == CTR_NOTEON )
       {
-        long int vel = ((long int)vmax[ch]*127)/maxLevel[ch];
+        long int vel = ((long int)vmax[ch] * 127) / maxLevel[ch];
         //Serial.println(vel);
         if (vel < 5) vel = 5;
         if (vel > 127) vel = 127;
@@ -87,13 +87,13 @@ void loop() {
       {
         MIDI_noteOff(MIDI_CHANNEL, midiNotes[ch]);
         counter[ch] = 0;
-        digitalWrite(statusPin, LOW);
+        digitalWrite(ledPins[ch], LOW);
       }
     }
 
     // The signal from the piezo is a damped oscillation decaying with
-    // time constant 8-10ms. Prevent false retriggering by raising 
-    // trigger level when first triggered, then decaying it to the 
+    // time constant 8-10ms. Prevent false retriggering by raising
+    // trigger level when first triggered, then decaying it to the
     // threshold over several future samples.
     trigLevel[ch] = ((trigLevel[ch] * 19) + (thresholdLevel[ch] * 1)) / 20;
   }
@@ -111,14 +111,14 @@ void MIDI_setup()
 
 void MIDI_noteOn(int ch, int note, int velocity)
 {
-  MIDI_SERIAL.write( 0x90+((ch-1) & 0xF) );
+  MIDI_SERIAL.write( 0x90 + ((ch - 1) & 0xF) );
   MIDI_SERIAL.write( note & 0x7F );
   MIDI_SERIAL.write( velocity & 0x7F );
 }
 
 void MIDI_noteOff(int ch, int note)
 {
-  MIDI_SERIAL.write( 0x80+((ch-1) & 0xF) );
+  MIDI_SERIAL.write( 0x80 + ((ch - 1) & 0xF) );
   MIDI_SERIAL.write( note & 0x7F );
   MIDI_SERIAL.write( 0x01 );
 }
