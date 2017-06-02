f578f97dd06f61ab55331c80beb8d6730ce215ec
Andrew Burns <aburnsni@gmail.com>
2017-06-02 09:45:28 +0100

    Introduced ntp fetch of time and setup independent job/time updates

--- esp8266_support_counter.ino
+++ esp8266_support_counter.ino
@@ -1,8 +1,7 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
-
+#include <time.h>
 #include <ESP8266WiFi.h>
-// This library is already built in to the Arduino IDE
 #include "wifisettings.h"
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
@@ -10,7 +9,13 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);
 const char* host = "support.flemingfulton.org.uk";
 const char* url = "/node/14?tca=4ezZx6G1a00khifZ87eQzQfqN9uAew47G-ybgmL8aUI";
 
-const int refresh = 60000;
+// Variables for timings
+const int jobsInterval = 180000;  // Update interval for jobs - 3 minutes
+const int timeInterval = 30000;   // Update interval for time - 30 seconds
+
+unsigned long currentMillis = 0;
+unsigned long previousJobMillis = -jobsInterval;  // Set previous time as negative to allow imediate update
+unsigned long previousTimeMillis = -timeInterval; // Set previous time as negative to allow imediate update
 
 int find_text(String needle, String haystack, int from) {
   int foundpos = -1;
@@ -64,84 +69,110 @@ void setup() {
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
+
+  configTime(1 * 3600, 0, "pool.ntp.org", "time.nist.gov");
+  lcd.clear();
 }
 
-int value = 0;
-
 void loop() {
+  currentMillis = millis();
+  updateJobs();
+  updateTime();
+}
 
-  Serial.print("connecting to ");
-  Serial.println(host);
+void updateJobs() {
+  if (currentMillis - previousJobMillis >= jobsInterval) {
+    // save the last time you blinked the LED
+    Serial.print("connecting to ");
+    Serial.println(host);
 
-  // Use WiFiClient class to create TCP connections
-  WiFiClient client;
-  const int httpPort = 80;
-  if (!client.connect(host, httpPort)) {
-    Serial.println("connection failed");
-    lcd.clear();
-    lcd.print("Connection");
-    lcd.setCursor(0, 1);
-    lcd.print("Failed");
-
-  }
-
-  // We now create a URI for the request
-
-  Serial.print("Requesting URL: ");
-  Serial.println(url);
-  // This will send the request to the server
-  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
-               "Host: " + host + "\r\n" +
-               "Connection: close\r\n\r\n");
-  delay(500);
-
-  // Read all the lines of the reply from server and print them to Serial
-  while (client.available()) {
-    String line = client.readStringUntil('\r');
-
-    int start_loc = find_text("<html>", line, 0);
-    int end_loc = find_text("</html>", line, 0);
-    lcd.setCursor(0, 0);
-    if (start_loc > 0 && end_loc > 0)
-    {
-      //      Serial.println("subs:  ");
-      lcd.print("Jobs: ");
-      for (int i = start_loc + 6; i < end_loc; i++)
-      {
-        Serial.print(line[i]);
-        lcd.print(line[i]);
-      }
-      lcd.print("          ");
-
+    // Use WiFiClient class to create TCP connections
+    WiFiClient client;
+    const int httpPort = 80;
+    if (!client.connect(host, httpPort)) {
+      Serial.println("connection failed");
+      lcd.clear();
+      lcd.print("Connection");
       lcd.setCursor(0, 1);
-      for (int i = 0; i < 16; i++) {
-        lcd.print(" ");
-      }
-      //      Serial.println("");
-      //      lcd.setCursor(0, 1);
-      //      int start_loc2 = find_text("<b>", line, end_loc + 1);
-      //      int end_loc2 = find_text("</b>", line, end_loc + 1);
-      //      if (start_loc2 > 0 && end_loc2 > 0)
-      //      {
-      //        lcd.print("views: ");
-      //        Serial.println("views:");
-      //        for (int i = start_loc2 + 3; i < end_loc2; i++)
-      //        {
-      //          lcd.print(line[i]);
-      //          Serial.print(line[i]);
-      //        }
-      //    }
+      lcd.print("Failed");
     }
 
-    //       Serial.println(line);
+    // We now create a URI for the request
+
+    Serial.print("Requesting URL: ");
+    Serial.println(url);
+    // This will send the request to the server
+    client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+                 "Host: " + host + "\r\n" +
+                 "Connection: close\r\n\r\n");
+    delay(500);
+
+    // Read all the lines of the reply from server and print them to Serial
+    while (client.available()) {
+      String line = client.readStringUntil('\r');
+
+      //      int start_loc = find_text("<html>", line, 0);
+      //      int end_loc = find_text("</html>", line, 0);
+      int start_loc = find_text("<total>", line, 0);
+      int end_loc = find_text("</total>", line, 0);
+
+      lcd.setCursor(0, 0);
+      //Serial.println(line);
+      if (start_loc > 0 && end_loc > 0)
+      {
+        Serial.println("Jobs: ");
+        lcd.print("Jobs: ");
+        for (int i = start_loc + 7; i < end_loc; i++)
+        {
+          Serial.print(line[i]);
+          lcd.print(line[i]);
+        }
+        //      lcd.print("          ");
+
+        //        lcd.setCursor(0, 1);
+        //        for (int i = 0; i < 16; i++) {
+        //          lcd.print(" ");
+        //        }
+
+        int start_loc2 = find_text("<urgent>", line, end_loc + 1);
+        int end_loc2 = find_text("</urgent>", line, end_loc + 1);
+
+        lcd.setCursor(8, 0);
+        if (start_loc2 > 0 && end_loc2 > 0)
+        {
+          Serial.println("Urgent:");
+          lcd.print("Urg: ");
+          for (int i = start_loc2 + 8; i < end_loc2; i++)
+          {
+            Serial.print(line[i]);
+            lcd.print(line[i]);
+          }
+        }
+      }
+
+      //       Serial.println(line);
+    }
+
+    Serial.println();
+    Serial.println("closing connection");
+
+    previousJobMillis = currentMillis;
   }
+}
 
-  Serial.println();
-  Serial.println("closing connection");
+void updateTime() {
+  if (currentMillis - previousTimeMillis >= timeInterval) {
+    lcd.setCursor(0, 1);
+    time_t t = time(NULL);
+    lcd.print(ctime(&t));
+    previousTimeMillis = currentMillis;
+  }
+}
 
-  delay(10000);
+
+void flashLED() {
   lcd.noBacklight();
-  delay(refresh - 10000);
+  delay(1000);
   lcd.backlight();
   delay(1000);
   lcd.noBacklight();
